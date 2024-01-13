5242efdd62952d665ee717627162a12b5c6ef2cf
nikhilranjan01 <nikhilranjan2004@gmail.com>
2024-01-14 00:05:42 +0530

    updated

---
+++ date time full.py
@@ -0,0 +1,10 @@
+import datetime
+current_time = datetime.datetime.now()
+print("Print your input")
+print("Day :", current_time.day)
+print("Month :", current_time.month)
+print("Year:", current_time.year)
+print("Hour :", current_time.hour)
+print("Minute :", current_time.minute)
+print("Second :", current_time.second)
+print("Microsecond :", current_time.microsecond)
