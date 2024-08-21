07582891b793ef1fa6ada118af9b5dcbfde2a7b6
nikhilranjan01 <nikhilranjan2004@gmail.com>
2024-08-21 23:53:50 +0530

    New Update

---
+++ app.py
@@ -0,0 +1,8 @@
+#write a program in pythone to create a localhost based web app using steamlit and execution the following 
+#(a) display warning
+#(b) display info
+
+import streamlit as st
+st.title("Streamlit Warning and Info Display")
+st.warning("This is a warning message!")
+st.info("This is an info message!")
